71caa8db95970b933ffb070ab61808ec53ee5923
polsen99 <126027590+polsen99@users.noreply.github.com>
2026-06-22 11:33:56 -0700

    Import all submodules in __init__.py for xqc.experiment access

--- xenquaco/__init__.py
+++ xenquaco/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.1.0"
+
+from xenquaco import experiment
+from xenquaco import pixel_classification
+from xenquaco import figures
+from xenquaco import data_processing
+from xenquaco import ilastik_workflow
